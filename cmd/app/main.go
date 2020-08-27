b4057b80310930c285c15edee4d214682f9f1bb3
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-08-27 21:43:58 +0530

    Add initial HTTP server

---
+++ cmd/app/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Greet)
+
+	log.Println("Starting server :8000")
+
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed")
+	}
+}
